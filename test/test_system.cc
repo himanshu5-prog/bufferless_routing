e809ec7a39e361b85b4f2b43139c68c567901dbd
Himanshu Yadav <himanshu07319@gmail.com>
2024-05-24 02:00:49 -0500

    Updated System test

--- test/test_system.cc
+++ test/test_system.cc
@@ -5,6 +5,26 @@
 
 int main(){
     System s;
-    s.print();
+    unsigned int cycleCount;
+    int xDim;
+    int yDim;
+    unsigned int maxCycleCount = 80;
+
+    unsigned int currentCycle;
+    xDim = 4;
+    yDim = 4;
+    for (unsigned int currentCycle = 0; currentCycle < maxCycleCount; ++currentCycle){
+       //generate flits in router
+       s.generateInjectFlit();
+
+        for (int i=0; i < xDim; ++i){
+            for (int j=0; j < yDim; ++j){
+                if (s.isValidInjectFlit(i,j)){
+                    s.printRouterInputFlit(i,j);
+                }
+            }
+        }
+       s.incrementCycleCount();
+    }
     return 0;
 }
