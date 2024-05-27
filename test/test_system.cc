20da6848908783798477494f98c4afcdf0e54958
Himanshu Yadav <himanshu07319@gmail.com>
2024-05-27 02:10:53 -0500

    Update system test

--- test/test_system.cc
+++ test/test_system.cc
@@ -5,20 +5,18 @@
 
 int main(){
     System sys;
-    unsigned int maxCycleCount = 15;
+    unsigned int maxCycleCount = 16;
 
     unsigned int currentCycle;
    //sys.setDebugMode();
     sys.printRouterForbiddenList();
-    //TODO: Test #1: Create a test in which one router is injecting flit and see how routing is happening.
-    //TODO: Test #2: Two routers are injecting traffic
     for (unsigned int currentCycle = 0; currentCycle < maxCycleCount; ++currentCycle){
        //Check if any incoming flit reached its destination----------
        sys.acceptFlit();
        //------------------------------------------------------------
        //Check if core can generate the flit-------------------------
-       sys.generateInjectFlit_oneRouter();
-       //sys.generateInjectFlit();
+       //sys.generateInjectFlit_oneRouter();
+       sys.generateInjectFlit();
        //------------------------------------------------------------
        //Check if the injected flit can be considered for arbitration
        sys.processInputPort();
